d7ce98bea6c2c644a10252380b2fe4dc6af67d26
Suprit Vaidya <suprit.vaidya@gmail.com>
2026-07-05 12:03:10 +0200

    fix: retry health check for Render cold start

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Api } from '../../services/api';
-
+import { retry, delay, catchError } from 'rxjs/operators';
+import { timer } from 'rxjs';
+import { retryWhen, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +10,25 @@ import { Api } from '../../services/api';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home implements OnInit{
+export class Home implements OnInit {
 
   healthStatus = '';
   constructor(private api: Api) {}
+
   ngOnInit(): void {
-  this.api.getHealth().subscribe({
-    next: (res) => this.healthStatus = 'Backend is up',
-    error: () => this.healthStatus = 'Backend is down'
-  });
-}
-
-
-
+    this.healthStatus = 'Checking connection...';
+    this.api.getHealth().pipe(
+      retryWhen(errors =>
+        errors.pipe(
+          mergeMap((error, index) => {
+            if (index >= 5) throw error;
+            return timer(8000);
+          })
+        )
+      )
+    ).subscribe({
+      next: () => this.healthStatus = 'Backend is up',
+      error: () => this.healthStatus = 'Backend is down'
+    });
+  }
 }
